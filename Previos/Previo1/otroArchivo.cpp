8156f0674684f4abe055ee5c85f3c35ffac394bb
Gabriel González Rivera <gabriel.gonzalezrivera@ucr.ac.cr>
2024-03-15 21:14:33 -0600

    Agregando el código del  otroArchivo.cpp
    Signed-off-by: Gabriel González Rivera <gabriel.gonzalezrivera@ucr.ac.cr>

---
+++ Previos/Previo1/otroArchivo.cpp
@@ -0,0 +1,21 @@
+// Primer programa en C++
+
+/* se incluye iostream, que sirve para
+poder imprimir texto como salida, como
+el hola mundo. */
+# include <iostream>
+
+/* Se define el bloque main que 
+será la función principal del programa */
+int main() {  
+
+    /* Se utiliza std::cout para imprimir 
+    el Hola Mundo, estudiantes desde otro archivo!, en la consola, 
+    el operador<< tiene la función de enviar datos al std::cout*/
+    std::cout << "Hola Mundo, desde otro archivo!";
+
+    /* se retorna un valor entero de cero finalizando
+    la función main */
+    return 0;
+
+}
