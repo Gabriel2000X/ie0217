443ccd0bfc9579fa5ad3419d2d15cc2f7abcfe84
Gabriel González Rivera <gabriel.gonzalezrivera@ucr.ac.cr>
2024-03-15 22:31:27 -0600

    Actualizando el holaMundo
    Signed-off-by: Gabriel González Rivera <gabriel.gonzalezrivera@ucr.ac.cr>

--- Previos/Previo1/holaMundo.cpp
+++ Previos/Previo1/holaMundo.cpp
@@ -12,8 +12,10 @@ int main() {
     /* Se utiliza std::cout para imprimir 
     el Hola Mundo, estudiantes de ie0217! en la consola, 
     el operador<< tiene la función de enviar datos al std::cout*/
-    std::cout << "Hola Mundo, estudiantes de ie0217!";
-
+    std::cout << "Hola Mundo, estudiantes de ie0217!\n";
+    /* Se utiliza la siguiente línea para probar el funcionamiento
+    de git stash*/
+    std::cout << "Probando el stash";
     /* se retorna un valor entero de cero finalizando
     la función main */
     return 0;
