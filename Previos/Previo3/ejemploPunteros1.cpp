1358d81a1dc0098d028453fee1082fabdca29d07
Gabriel González Rivera <gabriel.gonzalezrivera@ucr.ac.cr>
2024-03-16 21:33:36 -0600

    Se añade el primer ejemplo de uso de punteros
    Signed-off-by: Gabriel González Rivera <gabriel.gonzalezrivera@ucr.ac.cr>

---
+++ Previos/Previo3/ejemploPunteros1.cpp
@@ -0,0 +1,42 @@
+// ejemplo de utilización de enumeraciones y defines
+
+/* se incluye iostream, en este caso se usa para
+imprimir texto de salida. */
+# include <iostream>
+
+/* Se utiliza namespace para no tener la necesida de escribir std::
+antes de elementos como cout y endl */
+using namespace std;
+
+
+/* Se define la función main que 
+será la función principal del programa */
+int main() {  
+    
+    /* Se define la variable var */
+    int var = 5;
+
+    /* Se declara la variable int* que corresponde a un puntero entero*/
+    int* pointVar;
+
+    /* Se le asigna el valor de la dirección en memoria de var a pointvar */
+    pointVar = &var;
+
+    /* Se realiza la impresión  del valor de var*/
+    cout << "var = " << var << endl;
+
+    /* Se realiza la impresión  del valor de &var*/
+    cout << "Adress of var (&var)= " << &var << endl;
+
+    /* Se realiza la impresión  del valor de pointVar que debe tener el valor de 
+    &var ya que contiene la dirección de memoria */
+    cout << "pointVar= " << pointVar << endl;
+
+    /* Se realiza la impresión del valor de var, ya que al colocar un asterisco antes de pointvar se 
+    accede a lo que está contenido en la dirección de memoria */
+    cout << "Content of the adress pointed to by pointVar (*pointVar) = " << *pointVar << endl;
+    
+    /* se retorna un valor entero de cero finalizando
+    la función main */
+    return 0;
+}
