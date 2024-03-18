239a44c3eba2119fd3972596c2151b0f79a974e8
Gabriel González Rivera <gabriel.gonzalezrivera@ucr.ac.cr>
2024-03-17 23:29:48 -0600

    Se incluye un código con los ejemplos del ciclo for
    Signed-off-by: Gabriel González Rivera <gabriel.gonzalezrivera@ucr.ac.cr>

---
+++ Previos/Previo2/ciclosFor.cpp
@@ -0,0 +1,42 @@
+// Ejemplo de ciclos for
+/* */
+
+/* se incluye iostream, en este caso se usa para
+imprimir texto de salida. */
+# include <iostream>
+
+/* Se utiliza namespace para no tener la necesida de escribir std::
+antes de elementos como cout y endl */
+using namespace std;
+
+/* Se define la función main que 
+será la función principal del programa */
+int main() {  
+    
+    
+    /* Se utiliza un ciclo for para que se impriman todos los números enteros
+    del uno al cinco, se inicia en 1 y se suma un número en cada iteración
+    hasta llegar a 5 */
+    for (int i = 1; i<= 5; ++i) {
+        
+        cout << i << " ";
+       
+    }
+
+        cout << "\n";
+    /* Se declara e inicializa un arreglo de numeros enteros */
+    int num_array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    /* Se utiliza un ciclo for para que se impriman todos los números presentes
+    en el arreglo creado anteriormente, el for recorre el arreglo*/
+    for (int n : num_array) {
+
+        cout << n << " ";
+
+    }
+
+
+    /* se retorna un valor entero de cero finalizando
+    la función main */
+    return 0;
+}
