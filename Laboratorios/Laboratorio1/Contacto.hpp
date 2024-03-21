8b45272706030e3bb8a2a47770373861e9a9e57d
Gabriel González Rivera <gabriel.gonzalezrivera@ucr.ac.cr>
2024-03-21 09:37:42 -0600

    Se incluye el archivo  Contacto.hpp creado durante el laboratorio 1
    Signed-off-by: Gabriel González Rivera <gabriel.gonzalezrivera@ucr.ac.cr>

---
+++ Laboratorios/Laboratorio1/Contacto.hpp
@@ -0,0 +1,24 @@
+
+// Si no está definido CONTACTO_HPP
+// este se define
+#ifndef CONTACTO_HPP
+#define CONTACTO_HPP 
+
+
+# include <iostream>
+# include <string>
+
+// se crea la estructura contacto
+// para almacenar los datos de nombre
+// y telefono de diferentes personas
+// en el contenedor de datos que es la 
+//estructura
+struct contacto
+{
+    std::string nombre;
+    std::string telefono; 
+};
+
+
+
+# endif
