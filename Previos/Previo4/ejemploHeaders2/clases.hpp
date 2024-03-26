f3bdaccfff9d3e2def613aedfa84b6ff7183dc43
Gabriel González Rivera <gabriel.gonzalezrivera@ucr.ac.cr>
2024-03-25 21:20:09 -0600

    Se incluye el archivo .hpp que define la estructura de las clases  del ejemplo
    Signed-off-by: Gabriel González Rivera <gabriel.gonzalezrivera@ucr.ac.cr>

---
+++ Previos/Previo4/ejemploHeaders2/clases.hpp
@@ -0,0 +1,19 @@
+//Archivo .hpp de declaración de clases, funciones y atributos
+
+/* Si no está definido PERSONA_HPP
+este se define */
+#ifndef CLASES_HPP
+#define CLASES_HPP
+
+/* Se declara la estructura de la clase Student, se
+declaran sus atributos y métodos */
+class Student {
+
+    public:
+        double marks;
+
+        Student (double m);
+        
+};
+
+#endif // CLASES_HPP
