c73a792a0164b4ade0b54a536928d39e2214caaa
Gabriel González Rivera <gabriel.gonzalezrivera@ucr.ac.cr>
2024-03-25 21:21:24 -0600

    Se incluye el archivo .cpp en donde se define la clase Student que se utilizará
    Signed-off-by: Gabriel González Rivera <gabriel.gonzalezrivera@ucr.ac.cr>

---
+++ Previos/Previo4/ejemploHeaders2/clases.cpp
@@ -0,0 +1,12 @@
+//Archivo donde se definen la clase y sus métodos y atributos
+
+/* Se incluye el archivo .hpp que contiene
+las declaraciones de las funciones, clases y atributos*/
+# include "clases.hpp" 
+
+/* Se define el funcionamiento del constructor de 
+Student*/
+Student::Student (double m) { 
+    marks = m;
+
+        }
