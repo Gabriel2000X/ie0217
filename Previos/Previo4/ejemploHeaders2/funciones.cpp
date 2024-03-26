f9bdb3240f1698798309b85f6ec913fa263ad42c
Gabriel González Rivera <gabriel.gonzalezrivera@ucr.ac.cr>
2024-03-25 21:22:23 -0600

    Se incluye el archivo.cpp donde se define la función calculateAverage que se utiliza
    Signed-off-by: Gabriel González Rivera <gabriel.gonzalezrivera@ucr.ac.cr>

---
+++ Previos/Previo4/ejemploHeaders2/funciones.cpp
@@ -0,0 +1,23 @@
+// Archivo donde se define el funcionamiento de la función calculateAverage
+
+/* se incluye funciones.hpp, donde se tiene la declaración de las
+funciones. */
+# include "funciones.hpp" 
+
+/* se incluye iostream, en este caso se usa para
+imprimir texto de salida. */
+#include <iostream>
+
+/* Se utiliza namespace para no tener la necesida de escribir std::
+antes de elementos como cout y endl */
+using namespace std;
+
+/* Se define el funcionamiento de la función calculateAverage,
+esta calcula el primedio de los dos valores de la clase estudiante 
+que se le dan*/
+void calculateAverage (Student s1, Student s2) { 
+    
+    double average = (s1.marks + s2.marks) / 2;
+    cout << "Average Marks = " << average << endl;
+
+}
