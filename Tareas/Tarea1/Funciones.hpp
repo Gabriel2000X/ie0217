d80b29fc1eb69cc4621f813be7b602d0b0c4682e
Gabriel González Rivera <gabriel.gonzalezrivera@ucr.ac.cr>
2024-03-27 12:51:55 -0600

    Se incluyen las declaraciones de las funciones mencionadas en el anterior commit, en Funciones.hpp y además se incluye la declaración de iniciarJuego, que es la función que inicializa el juego
    Signed-off-by: Gabriel González Rivera <gabriel.gonzalezrivera@ucr.ac.cr>

--- Tareas/Tarea1/Funciones.hpp
+++ Tareas/Tarea1/Funciones.hpp
@@ -9,5 +9,20 @@ este se define */
 #include "estructura.hpp"
 
 using namespace std;
+/* Se declara la función del selector de dificultad*/
+int selectorDificultad();
+
+/* Se declara la función que agrega palabras al diccionario, se pasa
+el segundo elemento con referencia a la función para poder modificarlo
+dentro de la función y que la modificación persista fuera de la función
+aunque no se retorne la variable*/
+void agregarPalabra(string array[], int &cantidadPalabras);
+
+/* Se delcara la función que muestra el diccionario, se pasa array como
+conts para que no sea modificable y no se necesita usar la referencia de 
+cantidadPalabras porque tampoco se modificará*/
+void mostrarDiccionario( const string array[], int cantidadPalabras);
+
+juego iniciarJuego( string array[], int cantidadPalabras, int dificultad );
 
 #endif
