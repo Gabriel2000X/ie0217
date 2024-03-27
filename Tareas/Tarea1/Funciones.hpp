b699c422b5f98724c790a88a03ace112257d967e
Gabriel González Rivera <gabriel.gonzalezrivera@ucr.ac.cr>
2024-03-27 05:53:34 -0600

    Se incluye  el archivo de encabezado de funciones inicial
    Signed-off-by: Gabriel González Rivera <gabriel.gonzalezrivera@ucr.ac.cr>

---
+++ Tareas/Tarea1/Funciones.hpp
@@ -0,0 +1,13 @@
+/*Si no está definido FUNCIONES_HPP
+este se define */
+
+#ifndef FUNCIONES_HPP
+#define FUNCIONES_HPP 
+
+
+#include <string>
+#include "estructura.hpp"
+
+using namespace std;
+
+#endif
