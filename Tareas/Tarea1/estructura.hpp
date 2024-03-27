14b2dbce6f3403e9fef7c1424fbfb90091686af3
Gabriel González Rivera <gabriel.gonzalezrivera@ucr.ac.cr>
2024-03-27 05:57:50 -0600

    Se incluye el archivo de encabezado de la estructura que se usará para almacenar el juego
    Signed-off-by: Gabriel González Rivera <gabriel.gonzalezrivera@ucr.ac.cr>

---
+++ Tareas/Tarea1/estructura.hpp
@@ -0,0 +1,24 @@
+
+// Si no está definido CONTACTO_HPP
+// este se define
+#ifndef ESTRUCTURA_HPP
+#define ESTRUCTURA_HPP 
+
+
+# include <iostream>
+# include <string>
+using namespace std;
+
+
+struct juego
+{
+    string palabra;
+    string palabraAdivinar;
+    int intentosMax;
+    int intentosActuales;
+     
+};
+
+
+
+# endif
