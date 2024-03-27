161a444d654fcb13cdeb5c7259c2284352df380f
Gabriel González Rivera <gabriel.gonzalezrivera@ucr.ac.cr>
2024-03-27 05:55:40 -0600

    Se añade el archivo de funciones.cpp inicial
    Signed-off-by: Gabriel González Rivera <gabriel.gonzalezrivera@ucr.ac.cr>

---
+++ Tareas/Tarea1/Funciones.cpp
@@ -0,0 +1,12 @@
+#include "Funciones.hpp"
+
+/* se incluye iostream, en este caso se usa para
+imprimir texto de salida. */
+#include <iostream> 
+#include <random> 
+
+/* Se utiliza namespace para no tener la necesida de escribir std::
+antes de elementos como cout y endl */
+using namespace std;
+
+
