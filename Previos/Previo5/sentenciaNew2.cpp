ea746ebb5c38fa7d0c3428359f7b0a9bae46c982
Gabriel González Rivera <gabriel.gonzalezrivera@ucr.ac.cr>
2024-04-01 02:57:15 -0600

    Se indenta el archivo sentenciaNew2.cpp
    Signed-off-by: Gabriel González Rivera <gabriel.gonzalezrivera@ucr.ac.cr>

--- Previos/Previo5/sentenciaNew2.cpp
+++ Previos/Previo5/sentenciaNew2.cpp
@@ -4,47 +4,47 @@ using namespace std;
 
 int main() {
 
-/*Se declara la variable global num*/
-int num;
+    /*Se declara la variable global num*/
+    int num;
 
-/*Se pide al usuario que ingrese un valor para num
-que en este caso es el numero de estudiantes*/
-cout << "Ingrese el número total de estudiantes: ";
-cin >> num;
+    /*Se pide al usuario que ingrese un valor para num
+    que en este caso es el numero de estudiantes*/
+    cout << "Ingrese el número total de estudiantes: ";
+    cin >> num;
 
-/* Se declara un puntero flotante*/
-float* ptr;
+    /* Se declara un puntero flotante*/
+    float* ptr;
 
-/*Se define que el puntero aloja las direcciones de memoria 
-de un arreglo flotante de una cantidad de numeros que se 
-introduce en el programa, se usa memoria dinámica*/
+    /*Se define que el puntero aloja las direcciones de memoria 
+    de un arreglo flotante de una cantidad de numeros que se 
+    introduce en el programa, se usa memoria dinámica*/
 
-ptr = new float[num];
+    ptr = new float[num];
 
-/*Se le pide al usuario que ingrese los valores
-que se almacenan en cada entrada del arreglo*/
-cout<<"Ingrese el GPA de los estudiantes." << endl; 
+    /*Se le pide al usuario que ingrese los valores
+    que se almacenan en cada entrada del arreglo*/
+    cout<<"Ingrese el GPA de los estudiantes." << endl; 
 
-/*Con un for se van almacenando los valores en 
-el arreglo y accesando al puntero con de-referencia*/
-for (int i = 0; i < num; ++i) {
+    /*Con un for se van almacenando los valores en 
+    el arreglo y accesando al puntero con de-referencia*/
+    for (int i = 0; i < num; ++i) {
 
-cout<<"Student " << i + 1  << ": ";
-cin >> *(ptr + i);
+        cout<<"Student " << i + 1  << ": ";
+        cin >> *(ptr + i);
 
-}
+    }
 
-cout << "\nDisplaying GPA of students." << endl;
+    cout << "\nDisplaying GPA of students." << endl;
 
-/*Con otro for se imprimen los valores registrados anteriormente*/
-for (int i = 0; i < num; ++i) {
+    /*Con otro for se imprimen los valores registrados anteriormente*/
+    for (int i = 0; i < num; ++i) {
 
-cout << "Student " << i + 1 << ": " << *(ptr + i) << endl;
+        cout << "Student " << i + 1 << ": " << *(ptr + i) << endl;
 
-}
+    }
 
-/*La memoria dinámica que usaba ptr se libera con delete*/
-delete[] ptr;
-return 0;
+    /*La memoria dinámica que usaba ptr se libera con delete*/
+    delete[] ptr;
+    return 0;
 
 }
