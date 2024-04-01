0bbe4fc678d264a61e7c235714e904b16f897c7f
Gabriel González Rivera <gabriel.gonzalezrivera@ucr.ac.cr>
2024-03-31 20:48:54 -0600

    Se indenta el archivo variablesMemoria.cpp
    Signed-off-by: Gabriel González Rivera <gabriel.gonzalezrivera@ucr.ac.cr>

--- Previos/Previo5/variablesMemoria.cpp
+++ Previos/Previo5/variablesMemoria.cpp
@@ -7,27 +7,27 @@ int globalVariable = 42;
 
 int main() {
 
-/*. Se almacena en el stack, esto porque es una variable local
-que se declara dentro de la función main. */
-int stackVariable = 10;
+    /*. Se almacena en el stack, esto porque es una variable local
+    que se declara dentro de la función main. */
+    int stackVariable = 10;
 
-/* Se almacena en el heap, esto debido a que se utiliza new, esto 
-hace que se devuelva un espacio para almacenar un entero y se 
-guarde en el heap. */
-int* heapVariable = new int(20);
+    /* Se almacena en el heap, esto debido a que se utiliza new, esto 
+    hace que se devuelva un espacio para almacenar un entero y se 
+    guarde en el heap. */
+    int* heapVariable = new int(20);
 
-/*Este código se almacena en el text segment*/
-std::cout << "Valor de globalVariable: " << globalVariable << std::endl; 
- std::cout << "Valor de stackVariable:" << stackVariable << std::endl;
-std::cout << "Valor de heapVariable: " << *heapVariable << std::endl;
+    /*Este código se almacena en el text segment*/
+    std::cout << "Valor de globalVariable: " << globalVariable << std::endl; 
+    std::cout << "Valor de stackVariable:" << stackVariable << std::endl;
+    std::cout << "Valor de heapVariable: " << *heapVariable << std::endl;
 
-/* Liberar la memoria asignada en el heap para que si luego
-otra parte del programa necesita el espacio de memoria que usaba
-heapVariable pueda usarlo. */
-delete heapVariable;
+    /* Liberar la memoria asignada en el heap para que si luego
+    otra parte del programa necesita el espacio de memoria que usaba
+    heapVariable pueda usarlo. */
+    delete heapVariable;
 
 
-return 0;
+    return 0;
 }
 
  
