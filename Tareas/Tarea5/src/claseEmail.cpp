42219a0b71ad679c517e1488c928c2e9ff75102e
Gabriel González Rivera <gabriel.gonzalezrivera@ucr.ac.cr>
2024-05-12 14:48:03 -0600

    Se añade la definición del método getDominioEmail
    Signed-off-by: Gabriel González Rivera <gabriel.gonzalezrivera@ucr.ac.cr>

--- Tareas/Tarea5/src/claseEmail.cpp
+++ Tareas/Tarea5/src/claseEmail.cpp
@@ -34,3 +34,25 @@ std::string ValidadorEmail::getExtensionCorreo(const std::string& mail) {
 }
 
 
+/*Se define el método getExtensionCorreo que obtiene la extensión contenida en 
+el correo que debe introducir el usuario. */
+std::string ValidadorEmail::getDominioCorreo(const std::string& mail) {
+
+    /*se utiliza size_t para guardar la posición de arroba que encuentra
+    .find en mail*/
+    size_t arrobaPosicion = mail.find('@');
+    
+    /*Se define el limite* inicial que empieza en la posición luego de la arroba*/
+    size_t limiteInicial = arrobaPosicion + 1;
+
+    /*Se define el limite final que es la diferencia entre la posición del primer punto
+    y la posición antes de la arroba*/
+    size_t limiteFinal = mail.find('.') - arrobaPosicion- 1;
+
+    /*Se guarda el dominio en la variable dominio con substr utilizando los límites del dominio*/
+    std::string dominio =  mail.substr(limiteInicial, limiteFinal);
+    
+    
+    return dominio;
+}
+
